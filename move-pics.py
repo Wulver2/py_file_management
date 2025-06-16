55bdd273e34acd07922ad868436ea63439a92cf1
Wulver2 <ross.shayla21@gmail.com>
2025-06-16 08:25:39 -0400

    Creates target folders(where photo and video files will be moved to) in the case that they do not already exists

--- move-pics.py
+++ move-pics.py
@@ -7,7 +7,7 @@ import platform
 
 # should allow anyone to use on their computer without having to change paths
 source = Path.home() / "Downloads"
-target = Path.home() / "Documents/Photos"
+target = Path.home() / "Documents/photos"
 EXTS = ("jpg", "png", "jpeg", "mov", "mp4", "mp3")
 # year/month/day
 DATE_PATTERN = r".*(20\d\d)-?([01]\d)-?([0123]\d).*"
@@ -51,15 +51,55 @@ def creation_date(path_to_file):
             timestamp = stat.st_mtime
     return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
 
+def getFolder(year, monthNumber):
+    if (monthNumber == "01"):
+        monthFolder = "01_January"
+    elif (monthNumber == "02"):
+        monthFolder = "02_February"
+    elif (monthNumber == "03"):
+        monthFolder = "03_March"
+    elif (monthNumber == "04"):
+        monthFolder = "04_April"
+    elif (monthNumber == "05"):
+        monthFolder = "05_May"
+    elif (monthNumber == "06"):
+        monthFolder = "06_June"
+    elif (monthNumber == "07"):
+        monthFolder = "07_July"
+    elif (monthNumber == "08"):
+        monthFolder = "08_August"
+    elif (monthNumber == "09"):
+        monthFolder = "09_September"
+    elif (monthNumber == "10"):
+        monthFolder = "10_October"
+    elif (monthNumber == "11"):
+        monthFolder = "11_November"
+    elif (monthNumber == "12"):
+        monthFolder = "12_December"
+  
+    return year + "/" + monthFolder
+
 
 def move_photos():
     for file in files:
-         if(is_photo(file)):
-              date = get_date(source, file)
-              year = date["year"]
-              month = date["month"]
-              
-              
+         if (is_photo(file)):
+            date = get_date(source, file)
+            year = date["year"]
+            month = date["month"]
+
+            if (year == "0" or month == "0"):
+                continue
+
+            folder = getFolder(year, month)
+            targetFolder = str(target) + "/" + folder
+
+            if (not os.path.exists(targetFolder)):
+                os.makedirs(targetFolder)
+
+            targetFile = targetFolder + "/" + file
+            if(not os.path.exists(targetFile)):
+                  print(targetFile)
+
 
 def main():
     move_photos()
