f58cae8344b175f3fba491c07512a917e5cbb87a
Wulver2 <ross.shayla21@gmail.com>
2025-06-16 08:09:57 -0400

    Gets the month and year of the files that we want removed, to be used when creating folders for these images/videos

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # py_file_management
+Python program to organize downloaded pictures and videos (PDFs and TXTs files later) using Alex Hyett's video(https://youtu.be/1dgnl7oCVTY?feature=shared) as a guide.
--- move-pics.py
+++ move-pics.py
@@ -1,10 +1,16 @@
-import os;
+import os
 from pathlib import Path
+import re
+import datetime
+import platform
+
 
 # should allow anyone to use on their computer without having to change paths
 source = Path.home() / "Downloads"
 target = Path.home() / "Documents/Photos"
 EXTS = ("jpg", "png", "jpeg", "mov", "mp4", "mp3")
+# year/month/day
+DATE_PATTERN = r".*(20\d\d)-?([01]\d)-?([0123]\d).*"
 
 files = os.listdir(source)
 
@@ -15,10 +21,42 @@ def is_photo(file):
             return True
     return False
 
+def get_date(folder, file):
+    matchObj = re.match(DATE_PATTERN, file)
+    if (matchObj):
+         year = matchObj.group(1)
+         month = matchObj.group(2)
+         print(year)
+         print(month)
+    else: 
+         dateCreated = creation_date(str(folder) + "/" + file)
+         matchObj = re.match(DATE_PATTERN, dateCreated)
+         year = matchObj.group(1)
+         month = matchObj.group(2)
+         print(year)
+         print(month)
+    return {"year": year, "month": month}
+         
+    
+def creation_date(path_to_file):
+    if platform.system() == 'Windows':
+        timestamp = os.path.getctime(path_to_file)
+    else:
+        stat = os.stat(path_to_file)
+        try:
+            timestamp = stat.st_birthtime
+        except AttributeError:
+            timestamp = stat.st_mtime
+    return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+
 def move_photos():
     for file in files:
          if(is_photo(file)):
-              print(file)
+              date = get_date(source, file)
+              year = date["year"]
+              month = date["month"]
+              
               
 
 def main():
